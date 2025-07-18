4f4e6da4f888f275d594dae773b5d8d4da6e1021
mkmcyber <maithakelvin6@gmail.com>
2025-07-18 21:45:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ <script>
+    function orderItem(name, price) 
+     const message = Hello MKM, I want to order: ${name} (Ksh ${price}).;
+      const phone = "0791398996";
+      const url = https://wa.me/254791398996?text=${encodeURIComponent(message)};
+      window.open(url, '_blank');
+
+  </script>
